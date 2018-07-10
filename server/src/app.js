3e6a3d7d19849b32a9657411366ddb98c619f4a4
Stefano Tambellini <tambellini.stefano94@gmail.com>
2018-07-10 12:55:15 +0200

    Modificata struttura cartelle

---
+++ server/src/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send(
+    [{
+      title: "Hello World!",
+      description: "Hi there! How are you?"
+    }]
+  )
+})
+
+app.listen(process.env.PORT || 8081)
