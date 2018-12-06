b6fa97572c2208a5b48ccba8644d4517f5cb0179
Steg626 <steg626@gmail.com>
2018-12-06 22:22:28 +0100

    Aggiunte variabili per connessione a database ElephantSQL in produzione

--- db/index.js
+++ db/index.js
@@ -1,11 +1,20 @@
 var { Pool } = require("pg");
 
-var pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT
-});
+var pool;
+
+if (process.env.ONLINE === "ON") {
+  pool = new Pool({
+    connectionString: "postgres://ktgkvfde:sWIz7ip0tNOcU1dMSXCwGbrmED7CzAv6@manny.db.elephantsql.com:5432/ktgkvfde"
+  });
+} else {
+  pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT
+  });
+}
+
 
 module.exports = pool;
