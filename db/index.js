efd639750956a9462413f975d40e5ff0880e6605
Steg626 <steg626@gmail.com>
2018-12-07 18:56:04 +0100

    Impostate variabili per produzione

--- db/index.js
+++ db/index.js
@@ -1,32 +1,19 @@
-/*var { Pool } = require("pg");
-
-var pool;
+var options;
 
 if (process.env.ONLINE === "ON") {
-  pool = new Pool({
-    connectionString: "postgres://ktgkvfde:sWIz7ip0tNOcU1dMSXCwGbrmED7CzAv6@manny.db.elephantsql.com:5432/ktgkvfde"
-  });
+  options = {
+    client: 'pg',
+    connection: "postgres://ktgkvfde:sWIz7ip0tNOcU1dMSXCwGbrmED7CzAv6@manny.db.elephantsql.com:5432/ktgkvfde"
+  }
 } else {
-  pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT
-  });
-}
-
-module.exports = pool;
-
-*/
-
-const options = {
-  client: 'pg',
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+  options = {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
+    }
   }
 }
 
