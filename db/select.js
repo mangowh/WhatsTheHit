f1d6f6cd02253a8c7d7c3f5d3c9bccee061bffb3
Steg626 <steg626@gmail.com>
2018-12-08 18:07:19 +0100

    Corretto select per accettare parametri

--- db/select.js
+++ db/select.js
@@ -2,8 +2,13 @@ const debug = require("debug")("whatsthehit:api/select")
 const knex = require("./index.js");
 
 module.exports = (req, res, next) => {
+
+  var dati = req.body;
+  delete dati.tabella;
+  debug(dati);
+
   knex.from(req.body["tabella"]).select("*")
-    .where({ "id": req.body["id"] })
+    .where(dati)
     .then((rows) => res.send(rows))
     .catch((err) => debug(err.stack));
 };
