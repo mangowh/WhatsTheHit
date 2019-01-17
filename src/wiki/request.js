f171bdf3949163973ba6179ed6cfc0af6d4e5d41
Steg626 <steg626@gmail.com>
2019-01-17 18:39:01 +0100

    Attivato modulo per richieste della nazionalità

--- src/wiki/request.js
+++ src/wiki/request.js
@@ -1,21 +1,34 @@
 const debug = require("debug")("whatsthehit:api"),
   wdk = require("wikidata-sdk"),
-  rp = require('request-promise')
+  rp = require('request-promise'),
+  { getCode, getName } = require('country-list'),
+  CountryLanguage = require('country-language');
+
 
 module.exports = (req, res, next) => {
+  var nome = unescape(req.query.name)
+
+  var tipo
+
   var url = wdk.searchEntities({
-    search: 'tiziano ferro',
-    limit: 3,
+    search: nome,
+    limit: 1,
     language: "it",
     format: 'json'
   })
 
   rp(url)
     .then((response) => {
-      var id = JSON.parse(response).search[0].id
+      var id
+
+      if (JSON.parse(response).search[0]) {
+        id = JSON.parse(response).search[0].id
+      } else {
+        throw new Error('error');
+      }
       var options = {
         "ids": id,
-        "languages": ['en', 'it'],
+        "languages": [tipo == "paese" ? "en" : "it"],
       }
       return options
     })
@@ -26,14 +39,25 @@ module.exports = (req, res, next) => {
     .then(response => {
       var entities = wdk.simplify.entities(JSON.parse(response).entities)
       var entity = Object.keys(entities)[0].toString()
-      var id = entities[entity].claims.P1412.toString().split(",")
-      debug(id)
+
+      var id
+      if (entities[entity].claims.P1412) {
+        id = entities[entity].claims.P1412.toString().split(",")
+        tipo = "lingua"
+      } else {
+        id = entities[entity].claims.P27.toString().split(",")
+        tipo = "paese"
+      }
+
+      if (id === "undefined") {
+        throw new Error();
+      }
+
       var options = {
         "ids": id,
-        "languages": ['en', 'it'],
+        "languages": [tipo == "paese" ? "en" : "it"],
       }
       return options
-      
     })
     .then(opt => {
       return wdk.getEntities(opt)
@@ -42,8 +66,17 @@ module.exports = (req, res, next) => {
     .then((response) => {
       var entities = wdk.simplify.entities(JSON.parse(response).entities)
       var entity = Object.keys(entities)[0].toString()
-      var lang = entities[entity].labels
 
-      res.send(lang)
+      var result = entities[entity].labels[tipo == "paese" ? "en" : "it"]
+      if (tipo === "lingua") {
+        res.send(result)
+      } else {
+        CountryLanguage.getLanguage(getCode(result), (err, language) => {
+          res.send(language.nativeName[0])
+        })
+      }
+    })
+    .catch(error => {
+      res.render("error")
     })
 }
